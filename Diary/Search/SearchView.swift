26d1507d81eb0ce2880944a0f34f934353fccb48
wodyddml2 <wodyddml4@naver.com>
2022-08-26 22:47:12 +0900

    Feat : UISearchController 구현
    (#21)

--- Diary/Search/SearchView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SearchView.swift
-//  Diary
-//
-//  Created by J on 2022/08/26.
-//
-
-import Foundation
--- Diary/Search/SearchViewController.swift
+++ Diary/Search/SearchViewController.swift
@@ -1,14 +1,45 @@
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: BaseViewController {
 
+    private lazy var tableView: UITableView = {
+       let view = UITableView()
+        view.delegate = self
+        view.dataSource = self
+        view.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.reusableIdentifier)
+        return view
+    }()
+    let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .red
     }
     
+    override func configureUI() {
+        
+        navigationItem.searchController = searchController
+        searchController.view.addSubview(tableView)
+    }
 
+    override func setConstraint() {
+        tableView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.top.equalTo(searchController.view.safeAreaLayoutGuide)
+        }
+    }
    
 
+}
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.reusableIdentifier, for: indexPath) as? SearchTableViewCell else {return UITableViewCell()}
+        cell.label.text = "Sss"
+        return cell
+    }
+    
+    
 }
